a69f65f72f9e2e4b7c74448ce255408b71099c4b
Bror Hjemgaard <bahjemga@uio.no>
2020-08-31 20:52:52 +0200

    xkcd

--- xkcdxkcd.py
+++ xkcdxkcd.py
@@ -1,35 +1,22 @@
-import sys
-from PIL import Image
+from PIL import Image as XKCD
 
-# pass the image as command line argument
-image_path = "xkcd.png"
-img = Image.open(image_path)
-
-# resize the image
-width, height = img.size
-aspect_ratio = height / width
-new_width = 120
-new_height = aspect_ratio * new_width * 0.55
-img = img.resize((new_width, int(new_height)))
-# new size of image
-# print(img.size)
-
-# convert image to greyscale format
-img = img.convert("L")
-
-pixels = img.getdata()
-
-# replace each pixel with a character from array
-chars = ["B", "S", "#", "&", "@", "$", "%", "*", "!", ":", "."]
-new_pixels = [chars[pixel // 25] for pixel in pixels]
-new_pixels = "".join(new_pixels)
-
-# split string of chars into multiple strings of length equal to new width and create a list
-new_pixels_count = len(new_pixels)
-ascii_image = [new_pixels[index : index + new_width] for index in range(0, new_pixels_count, new_width)]
-ascii_image = "\n".join(ascii_image)
-print(ascii_image)
-
-# write to a text file.
-with open("ascii_image.txt", "w") as f:
-    f.write(ascii_image)
+XKCDXKCD = XKCD.open("xkcd.png")
+XKCDXKCDXKCD, XKCDXKCDXKCDXKCD = XKCDXKCD.size
+XKCDXKCDXKCDXKCDXKCD = XKCDXKCDXKCDXKCD / XKCDXKCDXKCD
+XKCDXKCDXKCDXKCDXKCDXKCD = 120
+XKCDXKCDXKCDXKCDXKCDXKCDXKCD = XKCDXKCDXKCDXKCDXKCD * XKCDXKCDXKCDXKCDXKCDXKCD * 0.55
+XKCDXKCD = XKCDXKCD.resize((XKCDXKCDXKCDXKCDXKCDXKCD, int(XKCDXKCDXKCDXKCDXKCDXKCDXKCD)))
+XKCDXKCD = XKCDXKCD.convert("L")
+XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD = XKCDXKCD.getdata()
+XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD = ["X", "K", "C", "Ð", "D", "x", "k", "c", "d", "×", " "]
+XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD = [
+    XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD[XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD // 25] for XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD in XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD
+]
+XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD = "".join(XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD)
+XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD = len(XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD)
+XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD = [
+    XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD[XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD : XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD + XKCDXKCDXKCDXKCDXKCDXKCD]
+    for XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD in range(0, XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD, XKCDXKCDXKCDXKCDXKCDXKCD)
+]
+XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD = "\n".join(XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD)
+print(XKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCDXKCD)
