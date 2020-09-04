080168efca3bcbc6e4577f37e7caeaa43995caed
haakooto <hakonolav.torvik@gmail.com>
2020-09-04 11:34:53 +0200

    xkcd

---
+++ xkcd.py
@@ -0,0 +1,5 @@
+from numpy.random import randint as xkcd
+from bs4 import BeautifulSoup as xkcdxkcd
+from PIL.Image import open as xkcdxkcdxkcd
+from requests import get as xkcdxkcdxkcdxkcd
+from io import BytesIO as xkcdxkcdxkcdxkcdxkcd
