61cf68df30e95b9358b157deb3733ebb0c6cb87e
Bror Hjemgaard <bahjemga@uio.no>
2020-09-06 00:45:07 +0200

    xkcd

--- xkcd.py
+++
@@ -1,5 +0,0 @@
-from numpy.random import randint as xkcd
-from bs4 import BeautifulSoup as xkcdxkcd
-from PIL.Image import open as xkcdxkcdxkcd
-from requests import get as xkcdxkcdxkcdxkcd
-from io import BytesIO as xkcdxkcdxkcdxkcdxkcd
